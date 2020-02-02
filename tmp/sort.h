b24a3b5c9d2f636df601c283d930332af3b6d437
kael <cdcp-1795@hotmail.com>
2020-02-02 19:02:24 +0000

    task 9

--- tmp/sort.h
+++ tmp/sort.h
@@ -26,10 +26,13 @@ int partition(int a[], int l, int h, size_t s);
 void q_sort(int a[], int l, int h, size_t s);
 void d_l_list_swap_n(listint_t *a, listint_t *b, listint_t **list);
 void m_sort(int *a, int l, int m, int r, int *tmp);
-void m_d_g(int *a, int l, int r, int *tmp)
+void m_d_g(int *a, int l, int r, int *tmp);
+void r_sort(int *a, int s, int e);
+int find_biggest(int *a, size_t s);
 
 void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 #endif
