151a02e86ad17934585f594832c95eac558cc6c7
kael <cdcp-1795@hotmail.com>
2020-02-03 18:53:32 +0000

    task 11

--- tmp/sort.h
+++ tmp/sort.h
@@ -35,4 +35,5 @@ void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
